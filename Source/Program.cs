3b70eb518ebcc056420a5d552c3780b54c8df616
Slava Muhtarjants <slava.muhtarjants@ruswizards.com>
2016-06-06 13:44:49 +0300

    New branch created.

---
+++ Source/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Simple.OData.Client;
+
+using Weezlabs.Storgage.Model;
+using System.Data.Spatial;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^\d{5,10}$");
+
+            while (true)
+            {
+                //Console.WriteLine(regex.IsMatch(Console.ReadLine()));
+                regex.IsMatch("025813");
+            }
+        }
+    }
+}
